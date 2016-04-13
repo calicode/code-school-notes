7b43413bd34d89737c0e90ab8615b5107706cb50
Andrew Schmidthuber <aschmidthuber@gmail.com>
2016-04-12 19:55:20 -0500

    i broke it good

--- code-school-real_time_web_with_nodejs/hello_dog.js
+++ code-school-real_time_web_with_nodejs/hello_dog.js
@@ -34,7 +34,7 @@ logger.emit('error', 'Codes broke');
 */
 
 //Streams. This will happily overwrite whatever filename is specified in writeStream
-
+/*
 var fs = require('fs');
 var http = require('http');
 
@@ -58,3 +58,99 @@ request.on('end', function(){
 response.end('uploaded!');
 });
 }).listen(8080);
+
+*/
+
+// Chap 4 - modules
+
+// export/import basics just like reg js. 
+//custom_hello.js
+/*
+var hello = function() {
+	console.log('hello');
+};
+
+	module.exports = hello;
+
+//custom_goodbye.js
+
+exports.goodbye = function(){
+	console.log("bye");
+};
+
+//app.js
+var hello = require('./custom_hello');
+hello();
+var gb = require('./custom_goodbye');
+gb.goodbye();
+
+/* require without ./ checks, in order -
+/app/node_modules
+~/node_modules/ 
+/home/node_modules/
+then /node_modules/
+
+
+
+
+package.json
+needs to be created to define name, dependancies etc. Used  in my crud app to setup nodaemon also
+
+semantic versioning
+"connect": 1.8.7
+1 major version probably will change api
+8 minor might change api
+7 patch = wont change api
+
+
+can use ranges e.x "connect"
+:"~1" will fetch anything between 1.0.0 and 2.0.0 can also do ~1.8 etc
+*/
+
+
+//Chapter 5 - express
+
+var express = require('express');
+var app = express();
+var request= require('request');
+var url = require('url');
+
+
+//: indicates that username is dynamic
+app.get('/wiki/:searchString', function(req,response) {
+	var searchString= req.params.searchString;
+	options = {
+		protocol:"http:",
+		host: 'en.wikipedia.org',
+		pathname:'/w/api.php',
+		query:{format:"json",
+			action:"query",
+			generator:"search",
+			gsrnamespace:0,
+			gsrlimit:5,
+			gsrsearch:searchString,
+			prop:"extracts",
+			exintro:"",
+			explaintext:"",
+			exsentences:3,
+			
+			exlimit:"max"}
+	};
+	var wikiUrl = url.format(options);
+	console.log(wikiUrl);
+	request(wikiUrl,function(err,res,body){
+var wikiResults = JSON.parse(body);
+console.log(wikiResults.query.pages);
+response.locals = {title:searchString,pages:wikiResults.query.pages};
+response.render('wikiResults.ejs');
+
+
+	});
+});
+app.listen(8080);
+
+// not working due to twitter api changes since video    was produced. will modify . 1.1 api requires 
+// authentication i get the jist and this looks nicer than doing a json request with a huge url
+/* didn't want to l  ink mobile phone with twitter account so i just  used what I know of wiki api to 
+continue the course */
+
